1ca56a7071750a3bf30ab8e8aeb81d4600d8912f
Mikey <mgevarts@outlook.com>
2020-07-03 13:23:16 -0500

    yes

--- toRelease.js
+++ toRelease.js
@@ -1,10 +1,12 @@
-if (!window.location.protocol.includes("https")) return window.location.protocol = "https:"
-let url = new URL(window.location);
-if (url.pathname.lastIndexOf(".html") == url.pathname.length - 5) url.pathname += "/..";
-let arr = url.pathname.split("/");
-let split = [];
-for (let i of arr)
-    if (i != "")
-        split.push(i);
-let file = split[split.length - 1];
-window.location.href = `https://github.com/hieyou1/mikeylabfiles/releases/download/${file}/${file}`;
+if (!window.location.protocol.includes("https")) window.location.protocol = "https:";
+else {
+    let url = new URL(window.location);
+    if (url.pathname.lastIndexOf(".html") == url.pathname.length - 5) url.pathname += "/..";
+    let arr = url.pathname.split("/");
+    let split = [];
+    for (let i of arr)
+        if (i != "")
+            split.push(i);
+    let file = split[split.length - 1];
+    window.location.href = `https://github.com/hieyou1/mikeylabfiles/releases/download/${file}/${file}`;
+}
