e45cdb68c97fb283f2b7605dd246c22e79d54f8f
Mikey <mgevarts@outlook.com>
2020-07-03 10:06:31 -0500

    yes

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b6c23fbb8abea86e28b791be5fcde6b073d0a524dce2b1157f68412c431b4bfe
-size 15
+this is file thing
--- xpire/index.js
+++ xpire/index.js
@@ -1,3 +1,61 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:909aa8f0dfee04d181a49017dc26e84ba61c63b40dba0a7d6ee1d5db47a90ce3
-size 2341
+const service = "ngrok";
+(async () => {
+    const pathRes = require("path").resolve;
+    const fetch = require("node-fetch");
+    process.env.PATH = process.env.PATH.concat(`:${pathRes("./jre")}:${pathRes("./jre/bin")}`);
+    process.chdir("./mc");
+    const {spawn} = require("child_process");
+    let mcsrv = spawn("java", ["-Xmx1G", "-Xms1G", "-jar", "server.jar", "nogui"]);
+    mcsrv.stdout.on("data", (a) => {console.log(a.toString().trim())});
+    mcsrv.stderr.on("data", (a) => {console.log(a.toString().trim())});
+    mcsrv.on("close", () => {
+        process.exit(0);
+    })
+    process.stdin.pipe(mcsrv.stdin);
+    var url;
+    if (service == "ngrok") {
+        const ngrok = require("ngrok");
+        url = await ngrok.connect({
+            proto: "tcp",
+            addr: 1025,
+            region: process.env.ngrok_region,
+            authtoken: process.env.ngrok_token
+        });
+    } else if (service == "serveo") {
+        let urlgrab = () => {
+            return new Promise((resolve, reject) => {
+                try {
+                    let sshsrv = spawn("ssh", ["-o", "StrictHostKeyChecking=no", "-R", "1025:localhost:1025", "test@serveo.net"]);
+                    sshsrv.stdout.on("data", (data) => {
+                        data = require("uncolor")(data.toString().trim());
+                        if (data.includes("Forwarding TCP connections from ")) {
+                            resolve("tcp://".concat(data.replace("Forwarding TCP connections from ", "").trim()));
+                        }
+                        console.log(data);
+                    });
+                    sshsrv.stderr.on("data", (a) => {console.log(a.toString().trim())});
+                } catch (err) {
+                    return reject(err);
+                }
+            });
+        };
+        url = await urlgrab();
+    } else {
+        throw new Error(`${service} is not serveo or ngrok`);
+        process.exit(1);
+    }
+    console.log(url);
+    var res = await fetch("https://mikeylab-dns-gateway--letsrepl.repl.co/dns", {
+        method: "post",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            "token": process.env.SERVER_TOKEN,
+            "name": process.env.SERVER_DOT,
+            "url": url
+        })
+    });
+    res = await res.text();
+})();
+"";
